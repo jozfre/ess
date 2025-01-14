6a6c2a58c0522248b5794f1f731620fa9ad1d6af
jozfre <johanzafrijohar@gmail.com>
2025-01-14 20:05:01 +0800

    list of mosque spaces ui

--- admin/mosque-space/list-space.php
+++ admin/mosque-space/list-space.php
@@ -128,7 +128,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Staff</h1>
+            <h1 class="m-0">Mosque Spaces</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
